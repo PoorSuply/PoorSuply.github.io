039ef6075470543608f9257345d57b4a4522ff92
PoorSuply <115440689+PoorSuply@users.noreply.github.com>
2023-10-19 23:48:46 +0800

    Posttest5

---
+++ Posttest5/banners.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Okt 2023 pada 17.45
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banners`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `banners`
+--
+
+CREATE TABLE `banners` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `element` varchar(50) NOT NULL,
+  `path` varchar(50) NOT NULL,
+  `rarity` varchar(50) NOT NULL,
+  `release_date` date NOT NULL,
+  `light_cone` varchar(50) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `banners`
+--
+
+INSERT INTO `banners` (`id`, `name`, `element`, `path`, `rarity`, `release_date`, `light_cone`, `image_name`, `description`) VALUES
+(4, 'Huo-Huo', 'Wind', 'Abudance', '★★★★★', '2023-11-02', 'Belum diketahui', 'Character_Huohuo_Introduction.webp', 'Character healer yang memiliki side role support karena menambah attack tim'),
+(9, 'Jingliu', 'Ice', 'Destruction', '★★★★★', '2023-10-11', 'My Body Is My Sword', 'show.png', 'Character DPs Destruction yang meningkat kan damage dengan mengkonsumsi darah tim sebesar 4%');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `banners`
+--
+ALTER TABLE `banners`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
