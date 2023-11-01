a7c4b8e24bce0171f75889c52a30f26c65e9494c
PoorSuply <115440689+PoorSuply@users.noreply.github.com>
2023-11-01 17:53:49 +0800

    koneksi.php

--- Posttest5/koneksi.php
+++ Posttest5/koneksi.php
@@ -1,17 +1,14 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Ganti dengan username database Anda jika menggunakan selain root
-$password = ""; // Biasanya kosong untuk kredensial default MySQL
-$dbname = "banners"; // Ganti dengan nama database Anda
+$username = "root";
+$password = "";
+$dbname = "banners";
 
-// Buat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Periksa koneksi
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Set charset untuk mendukung karakter khusus
 $conn->set_charset("utf8mb4");
 ?>
