69ffa2468a46d0bde9a341e26241f86718fc3eac
PoorSuply <115440689+PoorSuply@users.noreply.github.com>
2023-11-01 17:50:24 +0800

    hapus.php

--- Posttest5/hapus.php
+++ Posttest5/hapus.php
@@ -1,21 +1,17 @@
 <?php
-include 'koneksi.php';  // Sesuaikan dengan file koneksi.php Anda.
+include 'koneksi.php';
 
-// Ambil ID banner yang akan dihapus
 if (isset($_GET['id'])) {
     $banner_id = $_GET['id'];
 
-    // Ambil nama file gambar untuk dihapus dari direktori "uploads"
     $result = $conn->query("SELECT image_name FROM banners WHERE id=$banner_id");
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $image_name = $row['image_name'];
 
-        // Hapus gambar dari direktori "uploads"
         unlink("uploads/$image_name");
-
-        // Hapus data banner dari database
+        
         $sql = "DELETE FROM banners WHERE id=$banner_id";
 
         if ($conn->query($sql) === TRUE) {
